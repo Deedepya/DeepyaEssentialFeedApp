4bf04fb26d9835fe8ffc51275685e2a8a944a047
DedeepyaSalla <salladedeepya@gmail.com>
2024-03-03 16:14:19 -0600

    Created iOS project through xcode

---
+++ EssentialFeediOSApp/EssentialFeediOSApp/EssentialFeediOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeediOSAppApp.swift
+//  EssentialFeediOSApp
+//
+//  Created by dedeepya reddy salla on 03/03/24.
+//
+
+import SwiftUI
+
+@main
+struct EssentialFeediOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
