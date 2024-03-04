18723197808e20c822a38f04a2ad1aeb2fea4458
DedeepyaSalla <salladedeepya@gmail.com>
2024-03-03 18:47:20 -0600

    1.As HTTPClient class just acts an abstract class ie., it does not have any implementation inside may be it looked as if we can replace it with protocol which makes code more simpler
    2.Refactored code to include all configuration at single place in makeSut, used appropriate names for tuple return type so we can avoid referencing with 0, 1
    set url's default value because not all test cases needs it mandatorily
    
    3.Along with client, passed url also in DI through constructor DI for Remote Feed Loader.
    
    a.Before we hardcoding URL within load function. If we hardcode it then this function will be useful only for this one Server. What if we need to facilitate same code for deve, staging, prid differently then we need to add if else conditions within this load function to declare those urls.
    
    b.It violates Solid principles. Instead if we can pass url to load function then this function can be used for any environment. As we have only one remote feed loader instance, we instead pass it to constructor injection instead of method injection
    
    c.But we have two options here - one is to pass url to HTTPClient's constructor injection and other is to pass to RemoteFeedLoader init

--- EssentialFeediOSApp/EssentialFeediOSAppTests/RemoteFeedLoaderTests.swift
+++ EssentialFeediOSApp/EssentialFeediOSAppTests/RemoteFeedLoaderTests.swift
@@ -8,20 +8,28 @@
 import XCTest
 
 class RemoteFeedLoader {
+    
+    let url: URL
+    let client: HTTPClient
+    
+    init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
     func load(client: HTTPClient) {
-        client.get(fromUrl: URL(string: "https://www.google.com/?client=safari"))
+        client.get(fromUrl: url)
     }
 }
 
-class HTTPClient {
-    func get(fromUrl url: URL?) {
-    }
+protocol HTTPClient {
+    func get(fromUrl url: URL?)
 }
 
 class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
     
-    override func get(fromUrl url: URL?) {
+    func get(fromUrl url: URL?) {
         requestedURL = url
     }
 }
@@ -29,15 +37,20 @@ class HTTPClientSpy: HTTPClient {
 final class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestURL() {
-        let client = HTTPClientSpy()
-        _ = RemoteFeedLoader()
+        let (_, client) = makeSUT()
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader()
+        let (sut, client) = makeSUT(url: URL(string: "https://apiurl")!)
         sut.load(client: client)
         XCTAssertNotNil(client.requestedURL)
+    }
+    
+    func makeSUT(url: URL = URL(string: "https://www.google.com/?client=safari")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let loader = RemoteFeedLoader(url: url, client: client)
+        
+        return (loader, client)
     }
 }
